11e65d8e20b96a141b3c63a0609785485df744b0
Hugo Perez Garcia <hugoperez80@hotmail.com>
2022-01-16 18:44:21 +0100

    final

--- src/api/user/user.routes.js
+++ src/api/user/user.routes.js
@@ -2,7 +2,7 @@ const userRoute = require("express").Router();
 const { createUser, login, logout, getUser } = require("./user.controller");
 const { validate } = require("../../utils/validator/validator");
 const {isAuth} =require("../../utils/middlewares/auth");
-userRoute.get("/", getUser);
+
 
 userRoute.get("/",[isAuth], getUser);
 userRoute.post("/register", validate("createUser"), createUser);
